23ae3460beb122e005a50a2d3f5d077730e113db
Justin Myers <justin.myers@principlestudios.com>
2022-01-22 15:15:14 -0800

    initial commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"Nunito Sans"', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        black: '#242424',
+      },
+      lineHeight: {
+        'tight': '1.05',
+      },
+      fontSize: {
+        '7xl': '6rem',
+      }
+    }
+  },
+  plugins: [],
+}
